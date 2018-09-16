ab4889076b41ac64425c418ceb76ee68044e1d85
Nathaniel Morihara <nathanielmorihara@gmail.com>
2018-09-16 15:00:46 -0400

    Enemies v0
    Enemies just spawn and move to the bottom. No collission detection yet
    
    Should the enemies loop around? Or disappear upon reaching the bottom of
    the screen?

--- src/entities/enemies.js
+++ src/entities/enemies.js
@@ -10,22 +10,40 @@ class Enemy extends Entity {
         super('Enemy', null, 25, 25, 150, Globals.colors.Red)
         console.log(`instantiated ${ this.toString() }`)
         this.x = 320
-        this.y = 200
+        this.y = 0
         this.missiles = []
         this.timeOfLastMissile = Date.now()
     }
 
     stateUpdate() {
         super.stateUpdate()
+
+        this.y += this.speed
+
+        if (this.hasReachedBottom()) {
+            //Looping around now. Should they disappear?
+            //Is this a "survivr the level" game? Or exterminate all enemies?
+            this.y = 0
+        }
     }
 
     renderUpdate() {
         super.renderUpdate()
     }
 
+    hasReachedBottom() {
+        let canvas = Globals.getCtx().canvas
+        return this.y > canvas.height
+    }
+
 }
 module.exports.Enemy = Enemy
 
-class BaseEnemy extends Enemy {}
+class BaseEnemy extends Enemy {
+    constructor() {
+        super()
+        this.speed = 5
+    }
+}
 module.exports.BaseEnemy = BaseEnemy
 
